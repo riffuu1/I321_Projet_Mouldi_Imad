a5f444602708452dc20601fe2cc227a396bd7996
riffuu1 <pj85gyl@eduvaud.ch>
2025-11-06 11:14:28 +0100

    refactor: route pizza

--- mypizza/routes/users.js
+++ mypizza/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/pizzas', function(req, res, next) {
+    res.json(mock-pizzas);
+})
+
 module.exports = router;
